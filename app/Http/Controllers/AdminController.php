95edded162a648d998304639f0392ffd1462b0fc
shinpaingmin <shinpaingmin@gmail.com>
2023-11-07 22:56:41 +0630

    added functions for profile edit page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Storage;
+use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\File;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
 
@@ -35,7 +38,8 @@ class AdminController extends Controller
         if(Hash::check($request->oldPassword, $dbPassword))
         {
             $data = [
-                'password' => Hash::make($request->newPassword)
+                'password' => Hash::make($request->newPassword),
+                'updated_at' => Carbon::now()
             ];
             User::where('id', Auth::user()->id)->update($data);
 
@@ -57,6 +61,34 @@ class AdminController extends Controller
         return view('admin.profile.edit');
     }
 
+    // update profile info
+    public function update(Request $request, $id) {
+        $this->accountValidation($request, $id);
+        $data = $this->getUserData($request);
+
+        // for image
+        if($request->hasFile('image')) {
+            /*
+                1. get old image name from db
+                2. check => delete if the old image exists in local storage
+                3. store new one in local storage
+            */
+            $dbImage = User::where('id', $id)->first();
+            $dbImage = $dbImage->image;
+
+            if(!empty($dbImage)) {
+                Storage::delete('public/' . $dbImage);
+            }
+
+            $fileName = uniqid() . $request->file('image')->getClientOriginalName();    // unique filename
+            $request->file('image')->storeAs('public', $fileName);  // store in local storage
+            $data['image'] = $fileName;     // store in db
+        }
+
+        User::where('id', $id)->update($data);
+        return redirect()->route('admin#details')->with(['updateSuccess' => 'Updated Successfully!']);
+    }
+
     // password validation function
     private function passwordValidation($request) {
         Validator::make($request->all(), [
@@ -64,5 +96,29 @@ class AdminController extends Controller
             'newPassword' => ['required', Password::min(8)->mixedCase()->numbers()->symbols(), 'different:oldPassword'],
             'confirmPassword' => ['required', 'same:newPassword']
         ])->validate();
+    }
+
+    // account validation
+    private function accountValidation($request, $id) {
+        Validator::make($request->all(), [
+            'name' => ['required'],
+            'email' => ['required', 'unique:users,email,' . $id],
+            'phone' => ['required', 'min_digits:9', 'max_digits:15'],
+            'address' => ['required'],
+            'gender' => ['required'],
+            'image' => [File::image()->max(1024)]
+        ])->validate();
+    }
+
+    // request user data
+    private function getUserData($request) {
+        return [
+            'username' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'gender' => $request->gender,
+            'updated_at' => Carbon::now()
+        ];
     }
 }
