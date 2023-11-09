476653931eb507158a3868f506b2189c57c22dcd
shinpaingmin <shinpaingmin@gmail.com>
2023-11-09 23:10:27 +0630

    added functions for product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Storage;
+use Carbon\Carbon;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -57,21 +58,56 @@ class ProductController extends Controller
     // direct edit page
     public function editPage($id) {
         $product = Product::where('product_id', $id)->first();
+
         $categories = Category::select('category_id', 'category_name')->get();
+
         return view('admin.product.edit', compact(['product', 'categories']));
+    }
+
+    // product update function
+    public function update(Request $request, $id) {
+        $this->productValidation($request, $id);
+
+        $data = $this->getProductData($request);
+
+        $data['updated_at'] = Carbon::now();
+
+        if($request->hasFile('image')) {
+            $dbImage = Product::where('product_id', $id)->first();
+
+            $dbImage = $dbImage->image;
+
+            Storage::delete('public/' . $dbImage);
+
+            $fileName = uniqid() . $request->file('image')->getClientOriginalName();
+
+            $request->file('image')->storeAs('public', $fileName);
+
+            $data['image'] = $fileName;
+        }
+
+        Product::where('product_id', $id)->update($data);
+
+        return redirect()->route('product#list')->with(['updateSuccess' => 'Updated Successfully!']);
     }
 
     // private functions
     // product validation function
-    private function productValidation($request) {
-        Validator::make($request->all(), [
-            'productName' => ['required', 'string', 'min:4', 'max:255', 'unique:products,product_name'],
+    private function productValidation($request, $id="") {
+        $validationRules = [
+            'productName' => ['required', 'string', 'min:4', 'max:255', 'unique:products,product_name,' . $id . ',product_id'],
             'categoryName' => ['required'],
             'description' => ['required', 'string', 'min:10'],
             'image' => ['required', File::image()->max(2048)],
             'price' => ['required', 'integer'],
             'waitingTime' => ['required', 'integer']
-        ])->validate();
+        ];
+
+        if (!empty($id)) {
+            $validationRules['image'] = [File::image()->max(2048)];     // for update page
+        }
+
+        Validator::make($request->all(), $validationRules)->validate();
     }
 
     // get product data function
