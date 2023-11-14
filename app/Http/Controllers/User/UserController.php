fd9b50e28d6d346c311759f458592639664bb822
shinpaingmin <shinpaingmin@gmail.com>
2023-11-14 22:32:32 +0630

    added functions for user home page

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    //
+    // direct user home page
+    public function home() {
+        $products = Product::orderBy('created_at', 'desc')->get();
+
+        return view('user.main.home', compact('products'));
+    }
 }
