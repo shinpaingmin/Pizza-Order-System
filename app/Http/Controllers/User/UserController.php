5d8291e443be1cc3107ebc160dce80d1b91dbf4d
shinpaingmin <shinpaingmin@gmail.com>
2023-11-15 22:45:39 +0630

    added new functions

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers\User;
 
+use Storage;
+use Carbon\Carbon;
+use App\Models\User;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
 {
@@ -13,5 +21,102 @@ class UserController extends Controller
         $products = Product::orderBy('created_at', 'desc')->get();
 
         return view('user.main.home', compact('products'));
+    }
+
+    // direct change password page
+    public function changePasswordPage() {
+        return view('user.profile.changePassword');
+    }
+
+    // change password function
+    public function changePassword(Request $request) {
+        $this->passwordValidation($request);
+
+        $user = User::select('password')->where('id', Auth::user()->id)->first();
+
+        $dbPassword = $user->password;
+
+        if(Hash::check($request->oldPassword, $dbPassword))
+        {
+            $data = [
+                'password' => Hash::make($request->newPassword),
+                'updated_at' => Carbon::now()
+            ];
+            User::where('id', Auth::user()->id)->update($data);
+
+            // Auth::logout();
+
+            return redirect()->route('user#changePasswordPage')->with(['updateSuccess' => 'Updated Successfully!']);
+        }
+
+        return redirect()->route('user#changePasswordPage')->with(['notMatch' => 'The old password does not match.']);
+    }
+
+    // direct profile edit page
+    public function editProfile() {
+        return view('user.profile.edit');
+    }
+
+    // update profile function
+    public function updateProfile($id, Request $request) {
+        $this->accountValidation($request, $id);
+        $data = $this->getUserData($request);
+
+        // for image
+        if($request->hasFile('image')) {
+            /*
+                1. get old image name from db
+                2. check => delete if the old image exists in local storage
+                3. store new one in local storage
+            */
+            $dbImage = User::where('id', $id)->first();
+            $dbImage = $dbImage->image;
+
+            if(!empty($dbImage)) {
+                Storage::delete('public/' . $dbImage);
+            }
+
+            $fileName = uniqid() . $request->file('image')->getClientOriginalName();    // unique filename
+            $request->file('image')->storeAs('public', $fileName);  // store in local storage
+            $data['image'] = $fileName;     // store in db
+        }
+
+        User::where('id', $id)->update($data);
+        return redirect()->route('user#editProfilePage')->with(['updateSuccess' => 'Updated Successfully!']);
+    }
+
+    // password validation function
+    private function passwordValidation($request) {
+        Validator::make($request->all(), [
+            'oldPassword' => ['required', Password::min(8)],
+            'newPassword' => ['required', Password::min(8)->mixedCase()->numbers()->symbols(), 'different:oldPassword'],
+            'confirmPassword' => ['required', 'same:newPassword']
+        ])->validate();
+    }
+
+    // account validation
+    private function accountValidation($request, $id="") {
+        $validationRules =  [
+            'name' => ['required'],
+            'email' => ['required', 'unique:users,email,' . $id],
+            'phone' => ['required', 'min_digits:9', 'max_digits:15'],
+            'address' => ['required'],
+            'gender' => ['required'],
+            'image' => [File::image()->max(1024)]
+        ];
+
+        Validator::make($request->all(), $validationRules)->validate();
+    }
+
+    // request user data
+    private function getUserData($request) {
+        return [
+            'username' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'gender' => $request->gender,
+            'updated_at' => Carbon::now()
+        ];
     }
 }
