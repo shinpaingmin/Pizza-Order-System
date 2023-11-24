36b9d57ba31771ce8122731a15cd3e6c7dd6cc03
shinpaingmin <shinpaingmin@gmail.com>
2023-11-24 23:24:54 +0630

    added contact controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    // direct to contact page
+    public function contactList() {
+        $contacts = Contact::when(request('searchKey'), function($query) {
+            $query->orWhere('name', 'like', '%' . request('searchKey') . '%')
+                    ->orWhere('email', 'like', '%' . request('searchKey') . '%')
+                    ->orWhere('subject', 'like', '%' . request('searchKey') . '%');
+        })
+        ->orderBy('created_at', 'desc')->paginate(3);
+
+        return view('admin.contact.list', compact('contacts'));
+    }
 }
